ae131e7fe7ca1fb490d516b15bf4a4f2bc87fdef
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-08-03 16:14:00 -0400

    Feat: adds root to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
-  resources :merchants, only: [:show] do
+  root to: 'welcome#index'
+  resources :merchants, only: %i[show index] do
     resources :dashboard, only: [:index]
     resources :items, except: [:destroy]
     resources :item_status, only: [:update]
-    resources :invoices, only: [:index, :show, :update]
+    resources :invoices, only: %i[index show update]
   end
 
   namespace :admin do
     resources :dashboard, only: [:index]
     resources :merchants, except: [:destroy]
     resources :merchant_status, only: [:update]
-    resources :invoices, except: [:new, :destroy]
+    resources :invoices, except: %i[new destroy]
   end
 end

b36c2f3decd3a67596c8023d77ed5ecb5f80cecb
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-08-07 17:28:31 -0400

    Refactor: moves test data, Test: adds user_story_5 tests

--- spec/features/discounts/show_spec.rb
+++ spec/features/discounts/show_spec.rb
@@ -2,50 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'discount show' do
   before :each do
-    @merchant1 = Merchant.create!(name: 'Hair Care')
-
-    @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
-    @customer_2 = Customer.create!(first_name: 'Cecilia', last_name: 'Jones')
-    @customer_3 = Customer.create!(first_name: 'Mariah', last_name: 'Carrey')
-    @customer_4 = Customer.create!(first_name: 'Leigh Ann', last_name: 'Bron')
-    @customer_5 = Customer.create!(first_name: 'Sylvester', last_name: 'Nader')
-    @customer_6 = Customer.create!(first_name: 'Herber', last_name: 'Kuhn')
-
-    @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2)
-    @invoice_2 = Invoice.create!(customer_id: @customer_1.id, status: 2)
-    @invoice_3 = Invoice.create!(customer_id: @customer_2.id, status: 2)
-    @invoice_4 = Invoice.create!(customer_id: @customer_3.id, status: 2)
-    @invoice_5 = Invoice.create!(customer_id: @customer_4.id, status: 2)
-    @invoice_6 = Invoice.create!(customer_id: @customer_5.id, status: 2)
-    @invoice_7 = Invoice.create!(customer_id: @customer_6.id, status: 1)
-
-    @item_1 = Item.create!(name: 'Shampoo', description: 'This washes your hair', unit_price: 10,
-                           merchant_id: @merchant1.id)
-    @item_2 = Item.create!(name: 'Conditioner', description: 'This makes your hair shiny', unit_price: 8,
-                           merchant_id: @merchant1.id)
-    @item_3 = Item.create!(name: 'Brush', description: 'This takes out tangles', unit_price: 5,
-                           merchant_id: @merchant1.id)
-    @item_4 = Item.create!(name: 'Hair tie', description: 'This holds up your hair', unit_price: 1,
-                           merchant_id: @merchant1.id)
-
-    @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 1, unit_price: 10,
-                                status: 0)
-    @ii_2 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_2.id, quantity: 1, unit_price: 8, status: 0)
-    @ii_3 = InvoiceItem.create!(invoice_id: @invoice_2.id, item_id: @item_3.id, quantity: 1, unit_price: 5, status: 2)
-    @ii_4 = InvoiceItem.create!(invoice_id: @invoice_3.id, item_id: @item_4.id, quantity: 1, unit_price: 5, status: 1)
-    @ii_5 = InvoiceItem.create!(invoice_id: @invoice_4.id, item_id: @item_4.id, quantity: 1, unit_price: 5, status: 1)
-    @ii_6 = InvoiceItem.create!(invoice_id: @invoice_5.id, item_id: @item_4.id, quantity: 1, unit_price: 5, status: 1)
-    @ii_7 = InvoiceItem.create!(invoice_id: @invoice_6.id, item_id: @item_4.id, quantity: 1, unit_price: 5, status: 1)
-
-    @transaction1 = Transaction.create!(credit_card_number: 203_942, result: 1, invoice_id: @invoice_1.id)
-    @transaction2 = Transaction.create!(credit_card_number: 230_948, result: 1, invoice_id: @invoice_3.id)
-    @transaction3 = Transaction.create!(credit_card_number: 234_092, result: 1, invoice_id: @invoice_4.id)
-    @transaction4 = Transaction.create!(credit_card_number: 230_429, result: 1, invoice_id: @invoice_5.id)
-    @transaction5 = Transaction.create!(credit_card_number: 102_938, result: 1, invoice_id: @invoice_6.id)
-    @transaction6 = Transaction.create!(credit_card_number: 879_799, result: 1, invoice_id: @invoice_7.id)
-    @transaction7 = Transaction.create!(credit_card_number: 203_942, result: 1, invoice_id: @invoice_2.id)
-
-    @discount_1 = @merchant1.discounts.create!(percentage_discount: 5, quantity_threshold: 20)
+    load_test_data
   end
 
   # User Story 4: Merchant Bulk Discount Show
@@ -61,5 +18,53 @@ RSpec.describe 'discount show' do
     expect(current_path).to eq(merchant_discount_path(@merchant1, @discount_1))
     expect(page).to have_content(@discount_1.percentage_discount)
     expect(page).to have_content(@discount_1.quantity_threshold)
+  end
+
+  #   User Story 5: Merchant Bulk Discount Edit
+
+  # As a merchant
+  # When I visit my bulk discount show page
+  # Then I see a link to edit the bulk discount
+  # When I click this link
+  # Then I am taken to a new page with a form to edit the discount
+  # And I see that the discounts current attributes are pre-populated in the form
+  # When I change any/all of the information and click submit
+  # Then I am redirected to the bulk discount's show page
+  # And I see that the discount's attributes have been updated
+  it 'has a link to edit each discount' do
+    visit merchant_discount_path(@merchant1, @discount_1)
+    expect(page).to have_link('Edit Discount')
+    click_link 'Edit Discount'
+    fill_in 'discount[percentage_discount]', with: 90
+    fill_in 'discount[quantity_threshold]', with: 9
+    click_button 'Submit'
+    expect(current_path).to eq(merchant_discount_path(@merchant1, @discount_1))
+    expect(page).to have_content('Discount was successfully updated.')
+    expect(page).to have_content('90')
+    expect(page).to have_content('9')
+  end
+
+  it 'can also support changing only one attribute' do
+    visit merchant_discount_path(@merchant1, @discount_1)
+    expect(page).to have_link('Edit Discount')
+    click_link 'Edit Discount'
+    fill_in 'discount[percentage_discount]', with: 90
+    click_button 'Submit'
+    expect(current_path).to eq(merchant_discount_path(@merchant1, @discount_1))
+    expect(page).to have_content('Discount was successfully updated.')
+    expect(page).to have_content('90')
+    expect(page).to have_content('20')
+  end
+
+  it 'supports a sad path on editing a discount' do
+    visit merchant_discount_path(@merchant1, @discount_1)
+    expect(page).to have_link('Edit Discount')
+    click_link 'Edit Discount'
+    fill_in 'discount[percentage_discount]', with: 'aa'
+    fill_in 'discount[quantity_threshold]', with: ''
+    click_button 'Submit'
+    expect(current_path).to eq(merchant_discount_path(@merchant1, @discount_1))
+    expect(page).to have_content('Percentage discount is not a number')
+    expect(page).to have_content('Quantity threshold is not a number')
   end
 end

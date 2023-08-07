be72cbc51b485fd79bee51a860731bf3c5423579
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-08-07 17:28:59 -0400

    Test: adds tests for new methods in model

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe InvoiceItem, type: :model do
-  describe "validations" do
+  describe 'validations' do
     it { should validate_presence_of :invoice_id }
     it { should validate_presence_of :item_id }
     it { should validate_presence_of :quantity }
     it { should validate_presence_of :unit_price }
     it { should validate_presence_of :status }
   end
-  describe "relationships" do
+  describe 'relationships' do
     it { should belong_to :invoice }
     it { should belong_to :item }
   end
 
-  describe "class methods" do
+  describe 'class methods' do
     before(:each) do
       @m1 = Merchant.create!(name: 'Merchant 1')
       @c1 = Customer.create!(first_name: 'Bilbo', last_name: 'Baggins')
@@ -23,7 +23,8 @@ RSpec.describe InvoiceItem, type: :model do
       @c5 = Customer.create!(first_name: 'Arwen', last_name: 'Undomiel')
       @c6 = Customer.create!(first_name: 'Legolas', last_name: 'Greenleaf')
       @item_1 = Item.create!(name: 'Shampoo', description: 'This washes your hair', unit_price: 10, merchant_id: @m1.id)
-      @item_2 = Item.create!(name: 'Conditioner', description: 'This makes your hair shiny', unit_price: 8, merchant_id: @m1.id)
+      @item_2 = Item.create!(name: 'Conditioner', description: 'This makes your hair shiny', unit_price: 8,
+                             merchant_id: @m1.id)
       @item_3 = Item.create!(name: 'Brush', description: 'This takes out tangles', unit_price: 5, merchant_id: @m1.id)
       @i1 = Invoice.create!(customer_id: @c1.id, status: 2)
       @i2 = Invoice.create!(customer_id: @c1.id, status: 2)
@@ -37,6 +38,36 @@ RSpec.describe InvoiceItem, type: :model do
     end
     it 'incomplete_invoices' do
       expect(InvoiceItem.incomplete_invoices).to eq([@i1, @i3])
+    end
+  end
+
+  describe 'instance methods' do
+    before(:each) do
+      @m1 = Merchant.create!(name: 'Merchant 1')
+      @discount = @m1.discounts.create!(quantity_threshold: 5, percentage_discount: 10)
+      @c1 = Customer.create!(first_name: 'Bilbo', last_name: 'Baggins')
+
+      @item_1 = Item.create!(name: 'Shampoo', description: 'This washes your hair', unit_price: 1000,
+                             merchant_id: @m1.id)
+      @invoice = Invoice.create!(customer_id: @c1.id, status: 2)
+      @invoice_item = InvoiceItem.create!(invoice_id: @invoice.id, item_id: @item_1.id, quantity: 10, unit_price: 1000,
+                                          status: 0)
+    end
+
+    it '#unit_price_in_dollars' do
+      expect(@invoice_item.unit_price_in_dollars).to eq(10.0)
+    end
+
+    it '#applicable_discount' do
+      expect(@invoice_item.applicable_discount).to eq(@discount)
+    end
+
+    it '#discounted_unit_price' do
+      expect(@invoice_item.discounted_unit_price).to eq(900.0)
+    end
+
+    it '#revenue' do
+      expect(@invoice_item.revenue).to eq(9000.0)
     end
   end
 end

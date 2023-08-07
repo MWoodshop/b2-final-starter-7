e361b12406ef745ca07afcfba6f7f3dea66a4198
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-08-07 17:25:37 -0400

    Refactor: adds conversion to dollar methods

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -8,10 +8,33 @@ class InvoiceItem < ApplicationRecord
   belongs_to :invoice
   belongs_to :item
 
-  enum status: [:pending, :packaged, :shipped]
+  enum status: %i[pending packaged shipped]
 
   def self.incomplete_invoices
-    invoice_ids = InvoiceItem.where("status = 0 OR status = 1").pluck(:invoice_id)
+    invoice_ids = InvoiceItem.where('status = 0 OR status = 1').pluck(:invoice_id)
     Invoice.order(created_at: :asc).find(invoice_ids)
+  end
+
+  def unit_price_in_dollars
+    unit_price / 100.0
+  end
+
+  def applicable_discount
+    item.merchant.discounts
+        .where('quantity_threshold <= ?', quantity)
+        .order(percentage_discount: :desc)
+        .first
+  end
+
+  def discounted_unit_price
+    if discount = applicable_discount
+      unit_price - (unit_price * discount.percentage_discount / 100.0)
+    else
+      unit_price
+    end
+  end
+
+  def revenue
+    quantity * discounted_unit_price
   end
 end

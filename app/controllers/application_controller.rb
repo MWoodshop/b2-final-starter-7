864bde328b9af2eadb615f3f21bc25df61f1d628
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-08-08 16:45:37 -0400

    Feat: adds method to fetch API data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :get_next_three_holidays
+
+  def get_next_three_holidays
+    country_code = 'us'
+    url = "https://date.nager.at/api/v3/NextPublicHolidays/#{country_code}"
+
+    response = HTTParty.get(url)
+
+    if response.success?
+      @next_three_holidays = JSON.parse(response.body).first(3)
+    else
+      @error_message = "Error fetching next public holidays: #{response.code} - #{response.body}"
+      @next_three_holidays = []
+    end
+  end
 end

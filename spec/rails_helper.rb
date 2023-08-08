edb99c6252e889d047984839a1c394aec68cb620
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-08-08 12:09:46 -0400

    Test: adds code to clear tests before each RSpec run.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -40,6 +40,12 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+  config.before(:each) do
+    # Truncate all tables to clean up test data before each test
+    ActiveRecord::Base.connection.tables.each do |table|
+      ActiveRecord::Base.connection.execute("TRUNCATE #{table} RESTART IDENTITY CASCADE")
+    end
+  end
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
